867addc83cff84593146b0d819b939edf9d31412
vetalEducation <tyu96@ukr.net>
2021-04-01 13:17:44 +0300

    Added method showWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
+	public static void showWorld() {
+		System.out.print("Hello, world!");
+	}
+
 	public static void main(String[] args) {
-		System.out.print("Hello, world!!!");
+		showWorld();
 	}
 }
