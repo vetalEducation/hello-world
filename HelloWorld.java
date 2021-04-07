f0332fc3e67a44c32a8985393088219b772c58b7
vetalEducation <tyu96@ukr.net>
2021-04-07 11:54:51 +0300

    big refactored helloworld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,14 @@
 public class HelloWorld {
-	public static void showWorld() {
+	public static void sayHelloWorld() {
+		System.out.print("Hello, World!");
+	}
+
+	public static void saySomethingZelenskii() {
 		System.out.print("Зеля лох!");
 	}
 
 	public static void main(String[] args) {
-		showWorld();
+		sayHelloWorld();
+		saySomethingZelenskii();
 	}
 }
