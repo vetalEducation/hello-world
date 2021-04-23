d5f592bfa28d09db9c609daf5abbe74867d67f86
vetalEducation <tyu96@ukr.net>
2021-04-23 11:15:47 +0300

    Delete method saySomethingZelenskii().

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,12 +3,7 @@ public class HelloWorld {
 		System.out.print("Hello, World!");
 	}
 
-	public static void saySomethingZelenskii() {
-		System.out.print("Зеля лох!");
-	}
-
 	public static void main(String[] args) {
 		sayHelloWorld();
-		saySomethingZelenskii();
 	}
 }
