b71c209ff7e2e4994b9e5c4075ea8a8527b5c151
vetalEducation <tyu96@ukr.net>
2021-04-23 11:38:32 +0300

    Stayed case helloukraine by local repo

--- helloukraine.java
+++ helloukraine.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 public class HelloUkraine {
-=======
-public class helloukraine {
->>>>>>> b844007bd26125842e5ddb430482f6e119ab5325
 	public static void main(String[] args) {
 		System.out.println("Hello Ukraine!");
 	}
